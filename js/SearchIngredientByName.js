edb231384b163d04ce77f01543a18c2754490973
707Alex707 <alexandre707@live.com>
2020-03-04 10:58:41 -0500

    search by ing

--- js/SearchIngredientByName.js
+++ js/SearchIngredientByName.js
@@ -1,7 +1,7 @@
 //Pulls page for given term
 function lookupIngredient(){
-    var $searchTerm = document.getElementById("searchIngName").innerText;
-    console.log($searchTerm);
+    var $searchTerm = $('#searchIngName').val();
+    console.log("Search Term:"+ $searchTerm);
     $.ajax({
         url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?i="+ $searchTerm,
         success: function(data) {
@@ -13,10 +13,18 @@ function lookupIngredient(){
 
 function createListing(data){
     console.log(data);
-    console.log(data.ingredients[0]);
+    //console.log(data.ingredients[0]);
+
+    var html = "<table>";
 
     for(var i = 0; i < data.ingredients.length; i++){
+        console.log(data.ingredients[i]);
+
+        html += "<tr><td>Ingredient</td><td>" + data.ingredients[i].strIngredient + "</td><td>" +  data.ingredients[i].strDescription + "</td></tr>"
+
 
     }
 
+    html += "</table>";
+    document.getElementById("response").innerHTML = html;
 }
