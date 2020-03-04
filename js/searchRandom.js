6e02b4569f8911722f4dfcd01306f91febc4fde7
EthanBautista <43651657+EthanBautista@users.noreply.github.com>
2020-03-04 10:58:53 -0500

    Update searchRandom.js

--- js/searchRandom.js
+++ js/searchRandom.js
@@ -1,9 +1,18 @@
 
 
+
 $(document).ready(function() {
     $("#Random").click(function(){
         $.get( "https://www.thecocktaildb.com/api/json/v1/1/random.php", function( data ) {
-		    console.log(data);
-	    });	
+        console.log(data.drinks[0]);
+        var img = data.drinks[0].strDrinkThumb ;
+        console.log(data.drinks[0].strCategory);
+            $("#random").ready(function() {
+                console.log(img);
+                $("#random").attr("src", img);
+                document.getElementById('random').style.display='block';
+            });
+        });	
+        
     }); 
-});
+}); 
